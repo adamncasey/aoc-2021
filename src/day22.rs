3d657bef6420af13e6426c865d416e455968a1e5
Adam Casey <adamncasey@gmail.com>
2021-12-24 01:49:42 +0100

    Day 22

--- src/day22.rs
+++ src/day22.rs
@@ -1,41 +1,183 @@
-struct Instruction {
-    set_on: bool, // Otherwise set off
+use std::cmp::{min, max};
+
+#[derive(Debug, Clone)]
+struct Aabb {
+    on: bool,
     x_range: (i32, i32),
     y_range: (i32, i32),
     z_range: (i32, i32),
+    children: Vec<Aabb>, // non-overlapping
 }
 
-fn day22(instrs: Vec<Instruction>) -> usize {
-    let mut reactor = &mut [false; 101 * 101 * 101];
-    println!("{}", reactor.len());
-
-    for instr in instrs {
-        for x in instr.x_range.0..=instr.x_range.1 {
-            if x < -50 || x > 50 {
-                continue;
+impl Aabb {
+    fn num_squares(&self) -> (usize, usize) {
+        let total = self.calc_volume();
+        dbg!(total);
+        let mut total_lit = if self.on { total } else { 0 };
+        
+        for child in &self.children {
+            let (total, lit) = child.num_squares();
+            if self.on {
+                total_lit -= total;
             }
-            for y in instr.y_range.0..=instr.y_range.1 {
-                if y < -50 || y > 50 {
-                    continue;
-                }
-                for z in instr.z_range.0..=instr.z_range.1 {
-                    if z < -50 || z > 50 {
-                        continue;
-                    }
-                    let (x, y, z) = ((x + 50) as usize, (y + 50) as usize, (z + 50) as usize);
-                    //println!("{} {} {}", x, y, z);
-                    reactor[x * 101 * 101 + y * 101 + z] = instr.set_on;
+            total_lit += lit;
+        }
+        dbg!((total, total_lit));
+        (total, total_lit)
+    }
+
+    fn calc_volume(&self) -> usize {
+        (self.x_range.1 - self.x_range.0 + 1) as usize * (self.y_range.1 - self.y_range.0 + 1) as usize * (self.z_range.1 - self.z_range.0 + 1) as usize
+    }
+
+    fn does_intersect(&self, other: &Aabb) -> bool {
+        if self.x_range.0 <= other.x_range.1 && self.x_range.1 >= other.x_range.0 {
+            if self.y_range.0 <= other.y_range.1 && self.y_range.1 >= other.y_range.0 {
+                if self.z_range.0 <= other.z_range.1 && self.z_range.1 >= other.z_range.0 {
+                    return true;
                 }
             }
         }
+
+        false
     }
 
-    reactor.iter().filter(|x| **x).count()
+    /// Intersects `self` with `other`
+    ///
+    /// returns (other inside of self, list of Aabbs remaining outside of self)
+    fn intersect(&self, other: Aabb) -> (Option<Aabb>, Vec<Aabb>)
+    {
+        if !self.does_intersect(&other) {
+            return (None, vec![other]);
+        }
+
+        //let start_vol = other.calc_volume();
+
+        let intersection = Aabb {
+            on: other.on,
+            x_range: (max(self.x_range.0, other.x_range.0), min(self.x_range.1, other.x_range.1)),
+            y_range: (max(self.y_range.0, other.y_range.0), min(self.y_range.1, other.y_range.1)),
+            z_range: (max(self.z_range.0, other.z_range.0), min(self.z_range.1, other.z_range.1)),
+            children: Vec::new(),
+        };
+
+        let mut remaining = Vec::new();
+        if other.x_range.0 < self.x_range.0 {
+            println!("1");
+            // aabb of everything but x_range.1 clamped to self.x_range.0
+            let mut new = other.clone();
+            new.x_range.1 = self.x_range.0 - 1;
+            remaining.push(new);
+        }
+        if other.x_range.1 > self.x_range.1 {
+            println!("2");
+            // aabb of everything but x_range.0 clamped to self.x_range.1
+            let mut new = other.clone();
+            new.x_range.0 = self.x_range.1 + 1;
+            remaining.push(new);
+        }
+
+        if other.y_range.0 < self.y_range.0 {
+            println!("3");
+            // aabb of (self.x_range, y_range.1 clamped to self.y_range.0), other.z_range)
+            let mut new = other.clone();
+            new.x_range = intersection.x_range;
+            new.y_range.1 = self.y_range.0 - 1;
+            remaining.push(new);
+        }
+        if other.y_range.1 > self.y_range.1 {
+            println!("4");
+            // aabb of (self.x_range, y_range.0 clamped to self.y_range.1), other.z_range)
+            let mut new = other.clone();
+            new.x_range = intersection.x_range;
+            new.y_range.0 = self.y_range.1 + 1;
+            remaining.push(new);
+        }   
+        if other.z_range.0 < self.z_range.0 {
+            println!("5");
+            // aabb of (self.x_range, self.y_range, z_range.1 clamped to self.z_range.0)
+            let mut new = other.clone();
+            new.x_range = intersection.x_range;
+            new.y_range = intersection.y_range;
+            new.z_range.1 = self.z_range.0 - 1;
+
+            remaining.push(new);
+        }
+        if other.z_range.1 > self.z_range.1 {
+            println!("6");
+            // aabb of (self.x_range, self.y_range, z_range.0 clamped to self.z_range.1)
+            let mut new = other.clone();
+            new.x_range = intersection.x_range;
+            new.y_range = intersection.y_range;
+            new.z_range.0 = self.z_range.1 + 1;
+
+            remaining.push(new);
+        }
+        //println!("intersect yep {:?} {:?} {:?} {:?}", self, other, &intersection, &remaining);
+        
+        //dbg!((start_vol, intersection.calc_volume(), remaining.iter().map(|x| x.calc_volume()).sum::<usize>()));
+
+        (Some(intersection), remaining)
+    }
+
+    fn insert(&mut self, aabb: Aabb) -> Vec<Aabb> {
+        let (intersect, remaining) = self.intersect(aabb);
+
+        if let Some(intersect) = intersect {
+            let mut to_insert = vec![intersect];
+
+            for child in &mut self.children {
+                for inserting in to_insert.drain(..).collect::<Vec<Aabb>>() {
+                    to_insert.extend(child.insert(inserting));
+                }
+            }
+
+            self.children.extend(to_insert);
+        }
+
+        remaining
+    }
 }
 
-fn read_input(input: &str) -> Vec<Instruction> {
+fn day22(aabbs: Vec<Aabb>) -> usize {
+    let mut root_aabb = Aabb {
+        on: false,
+        x_range: (-50, 50),
+        y_range: (-50, 50),
+        z_range: (-50, 50),
+        children: Vec::new(),
+    };
 
-    let mut instructions = Vec::new();
+    for aabb in aabbs {
+        root_aabb.insert(aabb);
+    }
+
+    println!("{:?}", root_aabb);
+
+    root_aabb.num_squares().1
+}
+
+fn day22_2(aabbs: Vec<Aabb>) -> usize {
+    let mut root_aabb = Aabb {
+        on: false,
+        x_range: (-500000, 500000),
+        y_range: (-500000, 500000),
+        z_range: (-500000, 500000),
+        children: Vec::new(),
+    };
+    
+    for aabb in aabbs {
+        root_aabb.insert(aabb);
+    }
+
+    println!("{:?}", root_aabb);
+
+    root_aabb.num_squares().1
+}
+
+fn read_input(input: &str) -> Vec<Aabb> {
+
+    let mut aabbs = Vec::new();
 
     for instr in input.lines() {
         let (on_off, rest) = instr.split_once(" x=").unwrap();
@@ -48,15 +190,16 @@ fn read_input(input: &str) -> Vec<Instruction> {
         let ybounds = ys.split("..").map(|n| str::parse::<i32>(n).unwrap()).collect::<Vec<i32>>();
         let zbounds = zs.split("..").map(|n| str::parse::<i32>(n).unwrap()).collect::<Vec<i32>>();
 
-        instructions.push(Instruction {
-            set_on: on_off == "on",
+        aabbs.push(Aabb {
+            on: on_off == "on",
             x_range: (xbounds[0], xbounds[1]),
             y_range: (ybounds[0], ybounds[1]),
             z_range: (zbounds[0], zbounds[1]),
+            children: Vec::new(),
         });
     }
 
-    instructions
+    aabbs
 }
 
 #[test]
@@ -66,9 +209,26 @@ on x=11..13,y=11..13,z=11..13
 off x=9..11,y=9..11,z=9..11
 on x=10..10,y=10..10,z=10..10";
 
-    let input = read_input(input);
+    let aabbs = read_input(input);
 
-    assert_eq!(day22(input), 39);
+    assert_eq!(day22(aabbs), 39);
+}
+
+#[test]
+fn day22_1_example1() {
+    let input = "on x=10..12,y=10..12,z=10..12";
+
+    let aabbs = read_input(input);
+    assert_eq!(day22(aabbs), 27);
+}
+
+#[test]
+fn day22_1_example2() {
+    let input = "on x=10..12,y=10..12,z=10..12
+on x=11..13,y=11..13,z=11..13";
+
+    let aabbs = read_input(input);
+    assert_eq!(day22(aabbs), 46);
 }
 
 #[test]
@@ -78,4 +238,136 @@ fn day22_actual() {
     let input = read_input(&input);
 
     assert_eq!(day22(input), 580098);
+}
+
+#[test]
+fn day22_2_example() {
+    let input = "on x=-5..47,y=-31..22,z=-19..33
+on x=-44..5,y=-27..21,z=-14..35
+on x=-49..-1,y=-11..42,z=-10..38
+on x=-20..34,y=-40..6,z=-44..1
+off x=26..39,y=40..50,z=-2..11
+on x=-41..5,y=-41..6,z=-36..8
+off x=-43..-33,y=-45..-28,z=7..25
+on x=-33..15,y=-32..19,z=-34..11
+off x=35..47,y=-46..-34,z=-11..5
+on x=-14..36,y=-6..44,z=-16..29
+on x=-57795..-6158,y=29564..72030,z=20435..90618
+on x=36731..105352,y=-21140..28532,z=16094..90401
+on x=30999..107136,y=-53464..15513,z=8553..71215
+on x=13528..83982,y=-99403..-27377,z=-24141..23996
+on x=-72682..-12347,y=18159..111354,z=7391..80950
+on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
+on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
+on x=-52752..22273,y=-49450..9096,z=54442..119054
+on x=-29982..40483,y=-108474..-28371,z=-24328..38471
+on x=-4958..62750,y=40422..118853,z=-7672..65583
+on x=55694..108686,y=-43367..46958,z=-26781..48729
+on x=-98497..-18186,y=-63569..3412,z=1232..88485
+on x=-726..56291,y=-62629..13224,z=18033..85226
+on x=-110886..-34664,y=-81338..-8658,z=8914..63723
+on x=-55829..24974,y=-16897..54165,z=-121762..-28058
+on x=-65152..-11147,y=22489..91432,z=-58782..1780
+on x=-120100..-32970,y=-46592..27473,z=-11695..61039
+on x=-18631..37533,y=-124565..-50804,z=-35667..28308
+on x=-57817..18248,y=49321..117703,z=5745..55881
+on x=14781..98692,y=-1341..70827,z=15753..70151
+on x=-34419..55919,y=-19626..40991,z=39015..114138
+on x=-60785..11593,y=-56135..2999,z=-95368..-26915
+on x=-32178..58085,y=17647..101866,z=-91405..-8878
+on x=-53655..12091,y=50097..105568,z=-75335..-4862
+on x=-111166..-40997,y=-71714..2688,z=5609..50954
+on x=-16602..70118,y=-98693..-44401,z=5197..76897
+on x=16383..101554,y=4615..83635,z=-44907..18747
+off x=-95822..-15171,y=-19987..48940,z=10804..104439
+on x=-89813..-14614,y=16069..88491,z=-3297..45228
+on x=41075..99376,y=-20427..49978,z=-52012..13762
+on x=-21330..50085,y=-17944..62733,z=-112280..-30197
+on x=-16478..35915,y=36008..118594,z=-7885..47086
+off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
+off x=2032..69770,y=-71013..4824,z=7471..94418
+on x=43670..120875,y=-42068..12382,z=-24787..38892
+off x=37514..111226,y=-45862..25743,z=-16714..54663
+off x=25699..97951,y=-30668..59918,z=-15349..69697
+off x=-44271..17935,y=-9516..60759,z=49131..112598
+on x=-61695..-5813,y=40978..94975,z=8655..80240
+off x=-101086..-9439,y=-7088..67543,z=33935..83858
+off x=18020..114017,y=-48931..32606,z=21474..89843
+off x=-77139..10506,y=-89994..-18797,z=-80..59318
+off x=8476..79288,y=-75520..11602,z=-96624..-24783
+on x=-47488..-1262,y=24338..100707,z=16292..72967
+off x=-84341..13987,y=2429..92914,z=-90671..-1318
+off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
+off x=-27365..46395,y=31009..98017,z=15428..76570
+off x=-70369..-16548,y=22648..78696,z=-1892..86821
+on x=-53470..21291,y=-120233..-33476,z=-44150..38147
+off x=-93533..-4276,y=-16170..68771,z=-104985..-24507";
+
+    let aabbs = read_input(input);
+
+    assert_eq!(day22_2(aabbs), 2758514936282235);
+}
+
+#[test]
+fn day22_2_actual() {
+    let input = std::fs::read_to_string("./input/day22.txt").unwrap();
+
+    let input = read_input(&input);
+
+    assert_eq!(day22_2(input), 1134725012490723);
+}
+
+#[test]
+fn day22_aabb_intersect() {
+    let aabb = Aabb {
+        on: true,
+        x_range: (1,1),
+        y_range: (1,1),
+        z_range: (1,1),
+        children: Vec::new(),
+    };
+
+    let aabb2 = Aabb {
+        on: true,
+        x_range: (0,2),
+        y_range: (0,2),
+        z_range: (0,2),
+        children: Vec::new(),
+    };
+
+    let (intersect, remainder) = aabb.intersect(aabb2);
+
+    // [
+    //Aabb { on: true, x_range: (0, 0), y_range: (0, 2), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (2, 2), y_range: (0, 2), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (0, 0), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (2, 2), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (1, 1), z_range: (0, 0), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (1, 1), z_range: (2, 2), children: [] }
+
+    assert_eq!(intersect.unwrap().calc_volume(), 1);
+    assert_eq!(remainder.iter().map(|x| x.calc_volume()).sum::<usize>(), 26);
+}
+
+#[test]
+fn day22_aabb_intersect2() {
+    let aabb = Aabb { on: true, x_range: (10, 12), y_range: (10, 12), z_range: (10, 12), children: vec![] };
+    
+    let aabb2 = Aabb { on: true, x_range: (11, 13), y_range: (11, 13), z_range: (11, 13), children: vec![] };
+
+    let (intersect, remainder) = aabb.intersect(aabb2.clone());
+
+    // [
+    //Aabb { on: true, x_range: (0, 0), y_range: (0, 2), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (2, 2), y_range: (0, 2), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (0, 0), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (2, 2), z_range: (0, 2), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (1, 1), z_range: (0, 0), children: [] }, 
+    //Aabb { on: true, x_range: (1, 1), y_range: (1, 1), z_range: (2, 2), children: [] }
+
+    let remainder_vol = remainder.iter().map(|x| x.calc_volume()).sum::<usize>();
+    dbg!(&remainder);
+    let intersect_vol = intersect.unwrap().calc_volume();
+    assert_eq!(intersect_vol, 8);
+    assert_eq!(intersect_vol + remainder_vol, aabb2.calc_volume());
 }
